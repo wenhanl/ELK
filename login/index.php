1adda57ca10ec223a2b757803ee022e3d500167b
wenhanl <lvwenhan417@gmail.com>
2014-04-11 00:52:32 -0400

    login added

---
+++ login/index.php
@@ -0,0 +1,70 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Login</title>
+    </head>
+    
+    <body>
+
+ <?php 
+
+ if (isset($_POST['submit'])) { // if form has been submitted
+
+
+
+ // makes sure they filled it in
+
+ 	if(!$_POST['username'] || !$_POST['pass']) {
+
+ 		die('You did not fill in a required field.');
+
+ 	}
+
+ 	// checks it against the database
+
+ 	$check = $_POST['username'] == "admin" && $_POST['pass'] == "admin";
+
+ 	if($check){
+ 		header("Location: ./welcome.html");
+ 	}
+ 	else{
+
+ // if they are not logged in 
+
+ ?> 
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"> 
+
+ <table border="0"> 
+
+ <tr><td colspan=2><h1>Login</h1></td></tr> 
+
+ <tr><td>Username:</td><td> 
+
+ <input type="text" name="username" maxlength="40"> 
+
+ </td></tr> 
+
+ <tr><td>Password:</td><td> 
+
+ <input type="password" name="pass" maxlength="50"> 
+
+ </td></tr> 
+
+ <tr><td colspan="2" align="right"> 
+
+ <input type="submit" name="submit" value="Login"> 
+
+ </td></tr> 
+
+ </table> 
+
+ </form> 
+<?
+}
+}
+?>
+
+</body>
+</html>
